96737a03bd3542092f1ba5d541f34df9a5ed630a
zerospeedzero <zerospeedzero@gmail.com>
2023-10-25 01:23:26 -0600

    tailwind.config.js is updated

---
+++ pages/test1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const test1 = () => {
+  return (
+    <div className='w-screen h-screen mb-10'>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>testing</button>
+      <iframe src="https://w" title="description"></iframe>
+    </div>
+  )
+}
+
+export default test1
