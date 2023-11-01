c1348ea4d8ffd8f4b214361d74ba5a3a71fa2e69
zerospeedzero <zerospeedzero@gmail.com>
2023-11-01 13:06:35 -0600

    Make headline as component

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,11 @@
-import React, { useEffect } from 'react'
-import NewsGrid from '@/Components/NewsGrid'
-import Spinner from '@/Components/Spinner'
-import Official from '@/Components/Official'
-import { useRouter } from "next/router";
-import Cookies from 'js-cookie';
-
-function Home({vantaEffect}) {
-  const LOGIN_COOKIE_KEY = "login_attempts";
-  const router = useRouter();
-  const [news, setNews] = React.useState('')
-  const [showOfficial, setShowOfficial] = React.useState(false)
-  const showURL = (value) => {
-    setShowOfficial(value)
-  }
-  const getNews = async () => {
-    try {
-      // const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`)
-      const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=35af660db1a04e5a9bb061744398e4fd`)
-      const news = await res.json()
-      setTimeout(() => {
-        setNews(news)
-      }, 100)
-    } catch (error) {
-      console.log("Can get the news!")
-      setNews([])
-    }
-  }
-  useEffect(() => {
-    if (Cookies.get(LOGIN_COOKIE_KEY) !== "true") {
-      router.push("/login"); 
-    }
-  }, []);
-  useEffect(() => {
-      const news = getNews()
-  }, [])
+import React from 'react'
+import Home from '@/Components/Home'
+const index = () => {
   return (
     <>
-      <div className='h-16'></div>
-      {showOfficial && (<Official url={showOfficial} showURL={showURL} />)}
-      {news ? (
-        <NewsGrid heading="Headlines" articles={news.articles} showURL={showURL} vantaEffect={vantaEffect}/>
-      ) : (
-        <div>
-          <Spinner message='Loading News...'/>
-          <div className='h-screen'></div>
-        </div>
-      )
-      }
+      <Home />
     </>
   )
 }
 
-export default Home
+export default index
